c42e8f6c1682243e2e21fcd99b6f235d027eab5b
Geoff Schmit <gcschmit@gmail.com>
2017-01-24 22:35:03 -0600

    Updates for this year
    added class notes
    added more support to the summative lab

--- Drawing Editor Lab/DrawingPanel.java
+++ Drawing Editor Lab/DrawingPanel.java
@@ -1,4 +1,6 @@
+import javax.swing.JColorChooser;
 import javax.swing.JPanel;
+import java.awt.Color;
 
 /**
  * The panel in which draws all of the shapes in the drawing editor
@@ -8,6 +10,7 @@ import javax.swing.JPanel;
  */
 public class DrawingPanel extends JPanel
 {
+    Color fillColor;
     
 
     /**
@@ -17,5 +20,22 @@ public class DrawingPanel extends JPanel
     {
          
     }
+    
+    /**
+     * Invoked when the "Pick Color" button is clicked. Displays a JColorChooser and sets the
+     *  selected color as the new fill color. Leaves the fill color unchanged if the user clicks
+     *  "Cancel"
+     *
+     */
+    public void pickColor()
+    {
+        Color selectedColor = JColorChooser.showDialog( this, "select the fill color", this.fillColor );
+        
+        if( selectedColor != null )
+        {
+            this.fillColor = selectedColor;
+        }
+    }
+
     
 }
